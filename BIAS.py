91ed4219ed90bb105daf3f3021d07a6e0157ecba
Smart <smartgang@126.com>
2018-08-08 15:14:46 +0800

    [ADD]增加BIAS指标

--- BIAS.py
+++ BIAS.py
@@ -15,8 +15,12 @@ def bias(close, n):
 
 if __name__ == '__main__':
     import DATA_CONSTANTS as DC
-    N = 10
-    testdata = DC.getBarBySymbol('SHFE.RB', 'RB1810', 3600)
-    testdata['bias'] = bias(testdata['close'], 6)
-    testdata.to_csv('bias.csv')
+    domain_symbol = 'SHFE.RB'
+    bar_type = 3600
+    N = 40
+    symbolinfo = DC.SymbolInfo(domain_symbol)
+    bar_dic = DC.getBarDic(symbolinfo, bar_type)
+    domain_bar = DC.getDomainbarByDomainSymbol(symbolinfo.getSymbolList(), bar_dic, symbolinfo.getSymbolDomainDic())
+    domain_bar['bias'] = bias(domain_bar['close'], N)
+    domain_bar.to_csv('bias.csv')
     pass
